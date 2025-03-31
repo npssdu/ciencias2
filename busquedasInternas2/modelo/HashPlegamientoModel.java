74854eae8b7b16927633dd1693b9f306daf41ae3
Nelson David Posso S <ndpssz1313@gmail.com>
2025-03-30 22:20:28 -0500

    Función Plegamiento modificada

--- busquedasInternas2/modelo/HashPlegamientoModel.java
+++ busquedasInternas2/modelo/HashPlegamientoModel.java
@@ -8,25 +8,25 @@ public class HashPlegamientoModel extends HashModel {
 
     @Override
     protected int calcularHash(String clave) {
+        // Se define el tamaño del grupo como la longitud del máximo índice (tamanoTabla - 1)
+        int groupSize = String.valueOf(tamanoTabla - 1).length();
         int suma = 0;
-        int tamanoPieza = clave.length() / 2;
-        if (tamanoPieza == 0) {
+
+        // Se recorren la clave en grupos de "groupSize" dígitos
+        for (int i = 0; i < clave.length(); i += groupSize) {
+            int fin = Math.min(i + groupSize, clave.length());
+            String grupo = clave.substring(i, fin);
             try {
-                suma = Integer.parseInt(clave);
+                int num = Integer.parseInt(grupo);
+                suma += num;
             } catch (NumberFormatException e) {
-                return -1;
-            }
-        } else {
-            String parte1 = clave.substring(0, tamanoPieza);
-            String parte2 = clave.substring(tamanoPieza);
-            try {
-                int num1 = Integer.parseInt(parte1);
-                int num2 = Integer.parseInt(parte2);
-                suma = num1 + num2;
-            } catch (NumberFormatException e) {
+                // En caso de error, se retorna -1
                 return -1;
             }
         }
-        return suma % tamanoTabla + 1;
+        // Se le suma 1 al resultado de la suma de grupos
+        suma += 1;
+        // Se aplica el módulo para asegurar que el hash esté en el rango de la tabla
+        return suma % tamanoTabla;
     }
 }
